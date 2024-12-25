88e11678961406902c7e0e45cfab927cfba9996c
Nick Zalewski <nzalewsk@uwaterloo.ca>
2024-12-25 14:35:09 -0500

    Added general footer and header converted apple image to jpeg

---
+++ scripts/header.js
@@ -0,0 +1,7 @@
+let appHeader = `
+    <nav>
+       /*navigation or other html content here*/
+       <a href="/" title="HOME - The Nutrition Advantage">The Nutrition Advantage</a>
+    </nav>
+`;
+document.getElementById("header").innerHTML = appHeader;
