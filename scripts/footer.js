c991ae5e8da37a14b6d8366eefb64515c0646e2a
Nick Zalewski <nzalewsk@uwaterloo.ca>
2025-01-05 21:29:53 -0500

    Added services page and cleaned up overhead code

--- scripts/footer.js
+++
@@ -1,6 +0,0 @@
-let appFooter = `
-    <h1>The Nutrition Advantage</h1>
-    <p>cathy.ohalloran@sympatico.ca</p>
-    <p>416-441-1647</p>
-`;
-document.getElementById("footer").innerHTML = appFooter;
